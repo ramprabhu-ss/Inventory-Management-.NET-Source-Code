e6dce3cef1a76b900847185ee2cd00f9e170720e
ramprabhu-ss <ssramprabhu@gmail.com>
2026-07-19 15:13:51 +0530

    Add files via upload

--- DBScripts/MySql SP Scripts.sql
+++ DBScripts/MySql SP Scripts.sql
@@ -15,6 +15,18 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP TABLE IF EXISTS `todo_tasks`;
+CREATE TABLE `todo_tasks` (   
+`task_id` int NOT NULL AUTO_INCREMENT,  
+ `title` varchar(255) NOT NULL,   
+ `description` text,  
+ `end_date` date DEFAULT NULL,   
+ `completed` tinyint(1) DEFAULT '0',   
+ `created_at` datetime DEFAULT CURRENT_TIMESTAMP,   
+ `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  
+ PRIMARY KEY (`task_id`) 
+ ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ 
 -- New column 'employee_id' added for user_master table -- Start 
 SET @column_exists = (
     SELECT COUNT(*) 
